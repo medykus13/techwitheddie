5df48558d180c597b289376daac838ace7f05037
medykus13 <107961809+medykus13@users.noreply.github.com>
2025-08-15 06:14:13 +0200

    mqtt.sh

--- z2m.sh
+++ z2m.sh
@@ -1,84 +1,28 @@
-#!/bin/bash
-echo "----------------------------------------------------------------"
-echo "Setting Up Your Hub W/ Zigbee2MQTT - Ver 1.0"
-echo "----------------------------------------------------------------"
-echo " "
-# Node setup
-sudo curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
-sudo apt-get install -y nodejs git make g++ gcc
-echo ">>>>> Node Installed <<<<<"
-# Z2M setup
-echo ">>>>> Clone Zigbee2MQTT repository <<<<<"
-sudo mkdir /opt/zigbee2mqtt
-sudo git clone https://github.com/Koenkk/zigbee2mqtt.git /opt/zigbee2mqtt
-echo " "
-echo ">>>>> Install dependencies <<<<<"
-cd /opt/zigbee2mqtt
-git fetch
-git checkout master
-git pull
-npm ci
-echo " "
-echo ">>>>> Zigbee2MQTT Permission <<<<<"
-sudo chown -R ${USER}: /opt/zigbee2mqtt
-echo " "
-echo ">>>>> Create configuration.yaml <<<<<"
-cat > /opt/zigbee2mqtt/data/configuration.yaml <<EOL
-# Home Assistant integration (MQTT discovery)
-homeassistant: false
+#!/usr/bin/env bash
+APP="MQTT"
+var_tags="${var_tags:-mqtt}"
 
-# allow new devices to join
-permit_join: true
+header_info "$APP"
+variables
+color
+catch_errors
 
-# MQTT settings
-mqtt:
-  # MQTT base topic for Zigbee2MQTT MQTT messages
-  base_topic: zigbee2mqtt
-  # MQTT server URL
-  server: 'mqtt://192.168.8.169:1883'
-  # MQTT server authentication, uncomment if required:
-  # user: mqtt
-  # password: mqtt
+function update_script() {
+    header_info
+    check_container_storage
+    check_container_resources
+    if [[ ! -f /etc/mosquitto/conf.d/default.conf ]]; then
+        msg_error "No ${APP} Installation Found!"
+        exit
+    fi
+    msg_info "Updating ${APP} LXC"
+    $STD apt-get update
+    $STD apt-get -y upgrade
+    msg_ok "Updated Successfully"
+    exit
+}
 
-# Serial settings
-serial:
-  # Location of the adapter (see first step of this guide)
-  port: /dev/ttyUSB0
-
-frontend:
-  # Optional, default 8080 or you can use your own as well.
-  port: 8080
-  # IP address of the device running Zigbee2MQTT
-  host: 192.168.8.169
-advanced:
-  log_level: debug
-  network_key: GENERATE
-EOL
-echo " "
-echo ">>>>> configuration.yaml file created <<<<<"
-echo " "
-echo ">>>>> Enable daemon with systemctl <<<<<"
-cat > /etc/systemd/system/zigbee2mqtt.service <<EOL
-[Unit]
-Description=zigbee2mqtt
-After=network.target
-
-[Service]
-ExecStart=npm start
-WorkingDirectory=/opt/zigbee2mqtt
-StandardOutput=inherit
-# Or use StandardOutput=null if you don't want Zigbee2MQTT messages filling syslog, for more options see systemd.exec(5)
-StandardError=inherit
-Restart=always
-User=pi
-
-[Install]
-WantedBy=multi-user.target
-EOL
-echo " "
-echo "Zigbee2MQTT Installed"
-echo " "
-echo ">>>>> Start Zigbee2MQTT automatically on boot <<<<<"
-echo " "
-sudo systemctl enable zigbee2mqtt.service
-sudo reboot
+msg_ok "Completed Successfully!\n"
+echo -e "${CREATING}${GN}${APP} setup has been successfully initialized!${CL}"
+echo -e "${INFO}${YW} Access it using the following IP:${CL}"
+echo -e "${TAB}${GATEWAY}${BGN}${IP}:1883${CL}"
